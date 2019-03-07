f18c2fb212fca8d0aa9ff76ac5aa0f91fe68aa36
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-03-06 23:21:45 -0500

    d

--- graph.js
+++ graph.js
@@ -19,12 +19,36 @@ var drawChart=function(data)
   var width=400;
   var height=300;
   var barWidth=width/data.length;
-  var xScale = d3.scaleLinear()
-                 .domain([0, d3.max(data, function(d) { return d[0]; })])
-                 .range([0, w]);
+  var yScale = d3.scaleLinear()
+                 .domain([0, d3.max(data, function(d) { return d.starting; })])
+                 .range([0, height*3/4]);
   var svg=d3.select("div").append("svg")
             .attr("width",width)
             .attr("height",height)
             .style("margin-right",40)
             .style("background-color","#F7F7F6");
+
+            svg.selectAll("rect")
+               .data(data)
+               .enter()
+               .append("rect")
+               .attr("x",function(d,i){
+                 return i*barWidth+2;
+               })
+               .attr("y",function(d,i){
+                 //return height-d.num;
+                 return height-yScale(d.starting);
+               })
+               .attr("width",barWidth-4)
+               .attr("height",function(d){
+                 return yScale(d.starting);
+               })
+               .attr("fill",function(d){
+                 return "grey";
+               });
+               //.style("padding",2);
+
+
+
+
 }
